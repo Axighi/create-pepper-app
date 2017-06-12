224d20ffdb91ce3476bcd1b9bd97fa4e7fe4eaa4
Azriel <azriel.liang@axmtec.com>
2017-06-12 11:33:03 +0800

    v0.9.17

--- packages/react-scripts/template/src/routes/FullscreenImage/index.js
+++ packages/react-scripts/template/src/routes/FullscreenImage/index.js
@@ -2,10 +2,13 @@ import React from "react";
 import Fullscreen from "../../components/Fullscreen";
 import { connect } from "react-redux";
 
-const FullscreenImage = ({ image_url }) => (
+const FullscreenImage = ({ image_url }) =>
   <Fullscreen>
-    <img style={{ width: "100%", height: "100%" }} src={image_url} alt="" />
-  </Fullscreen>
-);
+    <img
+      style={{ width: "100%", height: "100%", zIndex: "100" }}
+      src={image_url}
+      alt=""
+    />
+  </Fullscreen>;
 
 export default connect(state => state.fullscreenImage)(FullscreenImage);
